c6af7f7d121350fea92edf749f2562cfd684e137
Killercrosx <96986315+Killercrosx@users.noreply.github.com>
2022-02-23 02:53:58 +0300

    Create Support.ts

---
+++ src/commands/General/Support.ts
@@ -0,0 +1,57 @@
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+
+import MessageHandler from "../../Handlers/MessageHandler";
+
+import BaseCommand from "../../lib/BaseCommand";
+
+import WAClient from "../../lib/WAClient";
+
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+
+  constructor(client: WAClient, handler: MessageHandler) {
+
+    super(client, handler, {
+
+      command: "support",
+
+      aliases: ["group","gc"],
+
+      description: "Gets the support group links",
+
+      category: "general",
+
+      usage: `${client.config.prefix}Support`,
+
+      baseXp: 10,
+
+    });
+
+  }
+
+  run = async (M: ISimplifiedMessage): Promise<void> => {
+
+    await this.client.sendMessage(
+
+      M.sender.jid,
+
+      ` _*I Am Angela🚀*_\n\n
+
+        _*𝐒𝐔𝐏𝐏𝐎𝐑𝐓*_:         
+
+*📮𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽 𝗟𝗶𝗻𝗸*
+
+https://chat.whatsapp.com/FNySjhEFYBqAjJtQiPzPIb
+
+`,
+
+      MessageType.text
+
+    );
+
+    return void M.reply("Sent you the Group Link in personal message");
+
+  };
+
+}
