95b19757c40c4b9e38b71210ec678b53e1d2f0aa
Killercrosx <96986315+Killercrosx@users.noreply.github.com>
2022-02-23 07:12:07 +0300

    Update Waifu.ts

--- src/commands/Weeb/Waifu.ts
+++ src/commands/Weeb/Waifu.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
           MessageType.image,
           undefined,
           undefined,
-          `*🌟 Here you go.*\n`,
+          `*Angela🚀*\n`,
           undefined
         ).catch((e) => {
           console.log(
