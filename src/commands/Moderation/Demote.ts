b6b42be4b4c8f4b81a6f8edc83908022d03f4080
Killercrosx <96986315+Killercrosx@users.noreply.github.com>
2022-02-23 06:17:00 +0300

    Update Demote.ts

--- src/commands/Moderation/Demote.ts
+++ src/commands/Moderation/Demote.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
 
   run = async (M: ISimplifiedMessage): Promise<void> => {
     if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-      return void M.reply(`How can I demote someone without being an admin?`);
+      return void M.reply(`Promote the Bot to use this Command!`);
     if (M.quoted?.sender) M.mentioned.push(M.quoted.sender);
     if (!M.mentioned.length)
       return void M.reply(`Tag the users you want to ${this.config.command}`);
