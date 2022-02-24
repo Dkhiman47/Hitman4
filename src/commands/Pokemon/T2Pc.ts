9f97beb6e40573b1e5f7b86e60ba3e8ef2304871
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2022-02-25 00:26:36 +0200

    Update T2Pc.ts

--- src/commands/Pokemon/T2Pc.ts
+++ src/commands/Pokemon/T2Pc.ts
@@ -53,7 +53,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `You might want to check your party.`,
-      footerText: "Angela🚀",
+      footerText: "hitman47🚀",
       buttons: buttons,
       headerType: 1,
     };
