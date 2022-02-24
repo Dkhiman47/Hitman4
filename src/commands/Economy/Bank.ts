66e8b2975feaccbfb472c6f43f95ee298448ba6f
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2022-02-25 00:21:07 +0200

    Update Bank.ts

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "Angela🚀",
+      footerText: "hitman47🚀",
       buttons: buttons,
       headerType: 1,
     };
