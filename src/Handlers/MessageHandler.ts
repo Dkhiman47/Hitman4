c7ca7b0f8db8cab45b9d977eb34cd61afbbe3f46
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2022-02-25 01:14:07 +0200

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -284,7 +284,7 @@ export default class MessageHandler {
       ];
       const buttonMessage: any = {
         contentText: `*A claimable character Appeared!*\n\n🎀 *Name: ${chara.name}*\n\n💬 *About:* ${chara.about}\n\n📛 *Source: ${source[0].anime.title}*\n\n💰 *Price: ${price}*\n\n*[Use ${this.client.config.prefix}claim to have this character in your gallery]*`,
-        footerText: "Angela🚀",
+        footerText: "hitman47🚀",
         buttons: buttons,
         headerType: 4,
         imageMessage: media?.message?.imageMessage,
@@ -304,7 +304,7 @@ export default class MessageHandler {
   };
 
   handleState = async (): Promise<void> => {
-    const text = `CONGRATS 🎉🎊 ANGELA🚀 IS CONNECTED BACK ONLINE!`;
+    const text = `CONGRATS 🎉🎊 hitman47🚀🌹 IS CONNECTED BACK ONLINE!`;
     await this.client.sendMessage(
       "120363037700352694@g.us",
       text,
