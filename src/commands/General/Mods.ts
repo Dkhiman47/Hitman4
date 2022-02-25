bc388a56c8e01041a0c5b3fafa5f8849eda070e0
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2022-02-25 19:11:05 +0200

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -1,33 +1,53 @@
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { ISimplifiedMessage } from "../../typings";
+import MessageHandler from '../../Handlers/MessageHandler'
+
+import BaseCommand from '../../lib/BaseCommand'
+
+import WAClient from '../../lib/WAClient'
+
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
-  constructor(client: WAClient, handler: MessageHandler) {
-    super(client, handler, {
-      command: "mods",
-      description: "Displays the Moderators' contact info",
-      category: "general",
-      usage: `${client.config.prefix}mods`,
-      aliases: ["moderators", "mod", "owner"],
-    });
-  }
 
-  run = async (M: ISimplifiedMessage): Promise<void> => {
-    if (!this.client.config.mods || !this.client.config.mods[0])
-      return void M.reply("*[UNMODERATED]*");
-    const filteredMap = this.client.config.mods
-      .map((mod) => this.client.getContact(mod))
-      .filter((user) => user);
-    let text = "⌈🏆hitman47🚀 𝙱𝚘𝚜𝚜🍀☘️⌋\n\n";
-    filteredMap.forEach(
-      (user, index) =>
-        (text += `#${index + 1}\n🎉🎊 *Username: ${
-          user.notify || user.vname || user.name || "null"
-        }*\n🍃🍂 *Contact: https://wa.me/+${user?.jid?.split("@")[0]}*\n\n`)
-    );
-    text += `\n hitman47🚀`;
-    return void M.reply(text);
-  };
+    constructor(client: WAClient, handler: MessageHandler) {
+
+        super(client, handler, {
+
+            command: 'mods',
+
+            description: "Displays the Moderators' contact info",
+
+            category: 'general',
+
+            usage: `${client.config.prefix}mods`,
+
+            aliases: ['moderators', 'mod', 'owner']
+
+        })
+
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+
+        if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
+
+        const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
+
+        let text = '🚀🌹 *My boss * 🚀🌹\n\n'
+
+        filteredMap.forEach(
+
+            (user, index) =>
+
+                (text += `#${index + 1}\n🎫 *Username: ${
+
+                    user.notify || user.vname || user.name || 'null'
+
+                }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+
+        )
+
+        return void M.reply(text)
+
+    }
+
 }
