463fcaa230025ee05c3c76402295b4df604ec924
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2022-02-26 05:47:42 +0200

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -42,7 +42,7 @@ export default class Command extends BaseCommand {
 
 *📮𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽 𝗟𝗶𝗻𝗸*
 
-https://chat.whatsapp.com/FNySjhEFYBqAjJtQiPzPIb
+https://chat.whatsapp.com/ChvkiST07BKGSgjX71nWQO
 
 `,
 
